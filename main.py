cbd34f3c78677594a2e895539cfdd722e140dbce
BuildTools <unconfigured@null.spigotmc.org>
2020-04-08 07:27:33 +0200

    It works!

--- main.py
+++ main.py
@@ -7,19 +7,19 @@ mapa = []
 
 class Predator(Creature):
     def __init__(self, x, y, rotation):
-        super().__init__(x, y, rotation, randint(1,4), random(), randint(1,10), randint(3,7), random(), [Herbivore()],[], random(), random())
+        super().__init__(x, y, rotation, randint(1,4), random(), randint(1,10), randint(3,7), random(), [Herbivore],[], random(), random())
     def moveing(self):
         self.move(randint(0,25),uniform(0, 2 * math.pi))
 
 class Herbivore(Creature):
     def __init__(self, x, y, rotation):
-        super().__init__(x, y, rotation, randint(1,4), random(), randint(1,10), randint(3,7), random(), [],[Predator()], random(), random())
+        super().__init__(x, y, rotation, randint(1,4), random(), randint(1,10), randint(3,7), random(), [],[Predator], random(), random())
     def moveing(self):
         self.move(randint(0,25),uniform(0, 2 * math.pi))
 
 class AppleTree(Plant):
     def __init__(self,x,y):
-        super().__init__(x, y, Apple(), randint(0,6), randint(30,50))
+        super().__init__(x, y, Apple, randint(0,6), randint(30,50))
 
 class Apple(Fruit):
     def __init__(self,x,y):
@@ -30,9 +30,8 @@ for x in range(mapaSize[0]):
     for y in range(mapaSize[1]):
         mapa[x].append([])
         a = random()
-        if a <= 1.1:
+        if a <= 0.1:
             print(x,y)
-            mapa[x][y].append(Creature(x, y, random(), randint(1,4), random(), randint(1,10), randint(3,7), random(), [Herbivore()],[], random(), random()))
             mapa[x][y].append(Predator(x,y,uniform(0, 2*math.pi)))
         elif a <= 0.2:
             print(x,y)
