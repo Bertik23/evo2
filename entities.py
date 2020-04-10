1d51a7e48bcb28499e538e92c5e90d94ec945d4d
bertik23 <bertikxxiii@gmail.com>
2020-04-10 13:28:39 +0200

    In the proces of making it work.

--- entities.py
+++ entities.py
@@ -1,9 +1,8 @@
 import math
 from random import choice, uniform, random, randint
-#from main import mapa
 
 class Creature:
-    def __init__(self, x, y, rotation, speed, moveEnergyCost, strength, viewDistance, viewAngle, foodList, enemyList, mutationChance, mutation, energy = 1):
+    def __init__(self, x, y, rotation, speed, moveEnergyCost, strength, viewDistance, viewAngle, foodList, enemyList, mutationChance, mutation, mapa, energy = 1):
         self.x = x
         self.y = y
         self.rotation = rotation
@@ -18,22 +17,24 @@ class Creature:
         self.mutation = mutation #what is the range of the mutation
         self.energy = energy #energy <0;1>
         self.age = 0
+        self.mapa = mapa
+        print(self.mapa)
     def move(self,distance, direction): #direction ↑ = 0, → = 0.5Pi
-        mapa[self.x][self.y].remove(self)
+        self.mapa[self.x][self.y].remove(self)
         if distance > self.speed:
             distance = self.speed
         self.rotation = direction
-        self.x += math.sin(direction) * distance
-        self.y += math.cos(direction) * distance
+        self.x += int(math.sin(direction) * distance)
+        self.y += int(math.cos(direction) * distance)
         self.energy -= distance * self.moveEnergyCost
-        mapa[self.x][self.y].append(self)
+        self.mapa[self.x][self.y].append(self)
     def eat(self, food):
         if type(food) in self.foodList:
             if food.strength >= self.strength:
                 self.energy -= self.strength / food.strength
             else:
                 self.energy += food.energy * (self.strength / food.strength)
-                mapa[self.x][self.y].remove(food)
+                self.mapa[self.x][self.y].remove(food)
     def reproduce(self, second):
         self.energy -= 0.3
         baby = type(self)(self.x, self.y, uniform(0, 2 * math.pi),
@@ -59,28 +60,29 @@ class Creature:
             baby.mutationChance *= 1 + uniform(-baby.mutation, baby.mutation)
         if baby.mutationChance <= random():
             baby.mutation *= 1 + uniform(-baby.mutation, baby.mutation)
-        mapa[self.x][self.y].append(baby)
+        self.mapa[self.x][self.y].append(baby)
     def lifeFunctions(self):
-        for entity in mapa[self.x][self.y]:
+        for entity in self.mapa[self.x][self.y]:
             if type(entity) in self.foodList:
                 self.eat(entity)
             if type(entity) == type(self) and entity != self and self.age > 30 and entity.age > 30 and self.energy > 0.5:
                 self.reproduce(entity)
-        if energy <= 0:
-            mapa[self.x][self.y].remove(self)
+        if self.energy <= 0:
+            self.mapa[self.x][self.y].remove(self)
 
 class Plant:
-    def __init__(self, x, y, fruit, spawnRange, spawnInterval):
+    def __init__(self, x, y, fruit, spawnRange, spawnInterval, mapa):
         self.x = x
         self.y = y
         self.fruit = fruit
         self.spawnRange = spawnRange
         self.spawnInterval = spawnInterval
         self.spawnCountdown = spawnInterval
+        self.mapa = mapa
     def spawn(self):
         x = self.x + randint(-spawnRange, spawnRange)
         y = self.y + randint(-spawnRange, spawnRange)
-        mapa[x][y].append(self.fruit(x,y))
+        self.mapa[x][y].append(self.fruit(x,y))
     def lifeFunctions(self):
         self.spawnCountdown -= 1
         if self.spawnCountdown <= 0:
@@ -88,10 +90,11 @@ class Plant:
             self.spawnCountdown = self.spawnInterval
 
 class Fruit:
-    def __init__(self, x, y, strength = 1, energy = 1,):
+    def __init__(self, x, y, mapa, strength = 1, energy = 1,):
         self.x = x
         self.y = y
         self.strength = strength
         self.energy = energy
+        self.mapa = mapa
     def lifeFunctions(self):
         pass
--- main.py
+++ main.py
@@ -1,29 +1,58 @@
 from entities import Creature, Fruit, Plant
 from random import randint, random, uniform
 import math
+import pprint
 
 mapaSize = (50,50)
 mapa = []
 
 class Predator(Creature):
     def __init__(self, x, y, rotation):
-        super().__init__(x, y, rotation, randint(1,4), random(), randint(1,10), randint(3,7), random(), [Herbivore],[], random(), random())
+        super().__init__(x, y, rotation, randint(1,4), random(), randint(1,10), randint(3,7), random(), [Herbivore],[], random(), random(), mapa)
     def moveing(self):
         self.move(randint(0,25),uniform(0, 2 * math.pi))
+    def __repr__(self):
+        return "P"
 
 class Herbivore(Creature):
     def __init__(self, x, y, rotation):
-        super().__init__(x, y, rotation, randint(1,4), random(), randint(1,10), randint(3,7), random(), [],[Predator], random(), random())
+        super().__init__(x, y, rotation, randint(1,4), random(), randint(1,10), randint(3,7), random(), [],[Predator], random(), random(), mapa)
     def moveing(self):
         self.move(randint(0,25),uniform(0, 2 * math.pi))
+    def __repr__(self):
+        return "H"
 
 class AppleTree(Plant):
     def __init__(self,x,y):
-        super().__init__(x, y, Apple, randint(0,6), randint(30,50))
+        super().__init__(x, y, Apple, randint(0,6), randint(30,50), mapa)
+    def __repr__(self):
+        return "T"
 
 class Apple(Fruit):
     def __init__(self,x,y):
-        super().__init__(x, y)
+        super().__init__(x, y, mapa)
+    def __repr__(self):
+        return "A"
+
+def pmap():
+    for i in mapa:
+        str = ""
+        for z in i:
+            if len(z) != 0:
+                for k in z:
+                    if k != None:
+                        str += k.__repr__()
+            else:
+                str += "_"
+        print(str)
+
+def run():
+    for i in mapa:
+        for j in i:
+            for k in j:
+                k.lifeFunctions()
+                if type(k) in [Predator,Herbivore]:
+                    k.moveing()
 
 for x in range(mapaSize[0]):
     mapa.append([])
@@ -31,13 +60,14 @@ for x in range(mapaSize[0]):
         mapa[x].append([])
         a = random()
         if a <= 0.1:
-            print(x,y)
             mapa[x][y].append(Predator(x,y,uniform(0, 2*math.pi)))
+            raise TypeError
         elif a <= 0.2:
-            print(x,y)
             mapa[x][y].append(Herbivore(x,y,uniform(0, 2*math.pi)))
         elif a <= 0.3:
-            print(x,y)
             mapa[x][y].append(AppleTree(x,y))
 
-print(mapa)
+for i in range(10):
+    pmap()
+    run()
+pmap()
